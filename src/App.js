a6c6382943a5c36247c25aff3ef33716e7073cb8
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-11-16 01:17:08 -0300

    actualizado el worker

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './style/index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Pages/router';
 
+import { Notifications } from 'react-push-notification';
 function App() {
   return (
     <div className="min-h-screen max-h-full min-w-screen max-w-screen text-2xl text-slate-900 bg-emerald-100">
       {/* ## ROUTER ## */}
+      <Notifications />
       <RouterProvider router={router} />
     </div>
   );
--- src/Pages/TodayTasks/TodayTasks.jsx
+++ src/Pages/TodayTasks/TodayTasks.jsx
@@ -3,12 +3,26 @@ import PlantItemOfList from '../../Components/Pure/PlantItemOfList';
 import { Link } from 'react-router-dom';
 //REDUX
 import { useSelector, useDispatch } from 'react-redux';
+
+import addNotification from 'react-push-notification';
+
 const TodayTasks = () => {
   const garden = useSelector((state) => state.garden);
-  // console.log(garden?.plants);
+  const buttonClick = () => {
+    addNotification({
+      title: 'Warning',
+      subtitle: 'This is a subtitle',
+      message: 'This is a very long message',
+      theme: 'darkblue',
+      native: true, // when using native, your OS will handle theming.
+    });
+  };
   return (
     <>
       <h3>Tareas para hoy: </h3>
+      <button onClick={buttonClick} className="button">
+        Hello world.
+      </button>
       <Link to="add">Crear</Link>
       <div className="flex flex-col gap-2 p-2">
         {garden
