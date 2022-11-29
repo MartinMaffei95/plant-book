54ba72025c785dc2dfc5e364a671984a0aa84447
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-11-29 13:52:24 -0300

    Fix bug: Notifications

--- src/Pages/TodayTasks/TodayTasks.jsx
+++ src/Pages/TodayTasks/TodayTasks.jsx
@@ -9,15 +9,39 @@ import { useState } from 'react';
 
 const TodayTasks = () => {
   const garden = useSelector((state) => state.garden.plants);
-  const buttonClick = () => {
-    addNotification({
-      title: 'Warning',
-      subtitle: 'This is a subtitle',
-      message: 'This is a very long message',
-      theme: 'darkblue',
-      native: true, // when using native, your OS will handle theming.
-    });
-  };
+  // const buttonClick = () => {
+  //   addNotification({
+  //     title: 'Warning',
+  //     subtitle: 'This is a subtitle',
+  //     message: 'This is a very long message',
+  //     theme: 'darkblue',
+  //     native: true, // when using native, your OS will handle theming.
+  //   });
+  // };
+
+  function notifyMe() {
+    if (!('Notification' in window)) {
+      // Check if the browser supports notifications
+      alert('This browser does not support desktop notification');
+    } else if (Notification.permission === 'granted') {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification('Hi there!');
+      // …
+    } else if (Notification.permission !== 'denied') {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          const notification = new Notification('Hi there!');
+          // …
+        }
+      });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
 
   // const [filter, setFilter] = useState('all');
 
@@ -28,8 +52,11 @@ const TodayTasks = () => {
     <>
       <h3>Tareas programadas para hoy: </h3>
       <div>
-        <button className="bg-red-400 m-2 p-2" onClick={buttonClick}>
+        {/* <button className="bg-red-400 m-2 p-2" onClick={buttonClick}>
           Enviar notifications
+        </button> */}
+        <button className="bg-red-400 m-2 p-2" onClick={notifyMe}>
+          Notify me!
         </button>
       </div>
       <select
