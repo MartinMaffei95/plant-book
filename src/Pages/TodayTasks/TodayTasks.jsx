64a97cf91ca67ffdd3efa9285849c35c559b277c
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-12-02 21:38:14 -0300

    test

--- src/Pages/TodayTasks/TodayTasks.jsx
+++ src/Pages/TodayTasks/TodayTasks.jsx
@@ -10,85 +10,9 @@ import { useState } from 'react';
 const TodayTasks = () => {
   const garden = useSelector((state) => state.garden.plants);
   const [notificationStatus, setNotificationStatus] = useState('');
-  // const buttonClick = () => {
-  //   addNotification({
-  //     title: 'Warning',
-  //     subtitle: 'This is a subtitle',
-  //     message: 'This is a very long message',
-  //     theme: 'darkblue',
-  //     native: true, // when using native, your OS will handle theming.
-  //   });
-  // };
-
-  function notifyMe() {
-    if (!('Notification' in window)) {
-      // Check if the browser supports notifications
-      alert('This browser does not support desktop notification');
-    } else if (Notification.permission === 'granted') {
-      // Check whether notification permissions have already been granted;
-      // if so, create a notification
-      const notification = new Notification('Hi there!');
-      // …
-    } else if (Notification.permission !== 'denied') {
-      // We need to ask the user for permission
-      Notification.requestPermission().then((permission) => {
-        // If the user accepts, let's create a notification
-        if (permission === 'granted') {
-          const notification = new Notification('Hi there!');
-          // …
-        }
-      });
-    }
-
-    // At last, if the user has denied notifications, and you
-    // want to be respectful there is no need to bother them anymore.
-  }
-
-  function checkNotificationPromise() {
-    try {
-      Notification.requestPermission().then();
-    } catch (e) {
-      return false;
-    }
-
-    return true;
-  }
-
-  function askNotificationPermission() {
-    // función para pedir los permisos
-    function handlePermission(permission) {
-      // configura el botón para que se muestre u oculte, dependiendo de lo que
-      // responda el usuario
-      if (
-        Notification.permission === 'denied' ||
-        Notification.permission === 'default'
-      ) {
-        setNotificationStatus('denied');
-      } else {
-        setNotificationStatus('granted');
-      }
-    }
-
-    // Comprobemos si el navegador admite notificaciones.
-    if (!('Notification' in window)) {
-      console.log('Este navegador no admite notificaciones.');
-    } else {
-      if (checkNotificationPromise()) {
-        Notification.requestPermission().then((permission) => {
-          handlePermission(permission);
-        });
-      } else {
-        Notification.requestPermission(function (permission) {
-          handlePermission(permission);
-        });
-      }
-    }
-  }
 
   // const [filter, setFilter] = useState('all');
 
-  // 2022-11-19T20:07:19.459Z
-  console.log(garden);
   const [filter, setFilter] = useState('ALL');
 
   function randomNotification() {
@@ -98,7 +22,8 @@ const TodayTasks = () => {
       // icon: notifImg,
     };
     var notif = new Notification(notifTitle, options);
-    setTimeout(randomNotification, 3000);
+    console.log(notif);
+    // setTimeout(randomNotification, 3000);
   }
 
   function sendNotif(e) {
@@ -115,36 +40,7 @@ const TodayTasks = () => {
       <div>
         <button onClick={sendNotif}>Notificarrrrr</button>
       </div>
-      <div>
-        {/* <button className="bg-red-400 m-2 p-2" onClick={buttonClick}>
-          Enviar notifications
-        </button> */}
-        <button className="bg-red-400 m-2 p-2" onClick={notifyMe}>
-          Notify me!
-        </button>
-      </div>
-      <div>
-        El usuario acepta notificaciones?
-        <div>
-          <button onClick={askNotificationPermission}>
-            Habilitar notificaciones
-          </button>
-          {notificationStatus ===
-          (
-            <button onClick={askNotificationPermission}>
-              Habilitar notificaciones
-            </button>
-          ) ? (
-            'NO SE PREGUNTO AUN'
-          ) : notificationStatus === 'granted' ? (
-            'SI'
-          ) : notificationStatus === 'denied' ? (
-            <button onClick={askNotificationPermission}>
-              Habilitar notificaciones
-            </button>
-          ) : null}
-        </div>
-      </div>
+
       <select
         name="filter"
         onChange={(e) => {
