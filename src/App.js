f4f7941adee534d0e8a26cf8574a2458a8c2cd8b
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-12-02 21:21:34 -0300

    test

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import router from './Pages/router';
 import { Notifications } from 'react-push-notification';
 import FooterContainer from './Components/Container/FooterContainer';
 import { schedulles } from './Models/configs/schedulles.enum';
-function App() {
+
+import { withServiceWorkerUpdater } from '@3m1/service-worker-updater';
+
+function App(props) {
+  const { newServiceWorkerDetected, onLoadNewServiceWorkerAccept } = props;
   localStorage.setItem('schedules', JSON.stringify(schedulles));
 
   return (
     <div className="min-h-screen max-h-full min-w-screen max-w-screen text-2xl text-slate-900 bg-mainColor-400">
       {/* ## ROUTER ## */}
       {/* <Notifications /> */}
+      {newServiceWorkerDetected ? (
+        <div>
+          Tenemos una nueva version. Instalala con click aca.{' '}
+          <button onClick={onLoadNewServiceWorkerAccept}>Update!</button>
+        </div>
+      ) : null}
       <RouterProvider router={router} />
     </div>
   );
 }
 
-export default App;
+export default withServiceWorkerUpdater(App);
