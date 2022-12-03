60e57e2faae0058ea8e01ed3833eaa9f517f7552
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-12-02 21:26:08 -0300

    test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './Store/store';
 
+import { onServiceWorkerUpdate } from '@3m1/service-worker-updater';
+
 //AUTH0
 import { Auth0Provider } from '@auth0/auth0-react';
 
@@ -27,7 +29,9 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: onServiceWorkerUpdate,
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
