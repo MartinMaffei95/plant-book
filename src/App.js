841bf8aef2660430288a589ce184363dc1c25760
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-12-05 17:52:28 -0300

    fix notificacitons

--- src/App.js
+++ src/App.js
@@ -10,17 +10,20 @@ import Updater from './Components/Updater/Updater';
 function App() {
   localStorage.setItem('schedules', JSON.stringify(schedulles));
 
-  console.log(navigator.serviceWorker.ready);
-  navigator.serviceWorker.ready.then(() => {
-    console.log('asd');
-    navigator.serviceWorker.controller.postMessage({
-      type: 'MENSAJE',
+  const msge = () => {
+    console.log(navigator.serviceWorker.ready);
+    navigator.serviceWorker.ready.then(() => {
+      console.log('asd');
+      navigator.serviceWorker.controller.postMessage({
+        type: 'MENSAJE',
+        payload: 'MENSAJE DE CONSOLA',
+      });
     });
-  });
-
+  };
   return (
     <div className="min-h-screen max-h-full min-w-screen max-w-screen text-2xl text-slate-900 bg-mainColor-400">
       <Updater />
+      <button onClick={msge}>MENSAJE</button>
       {/* ## ROUTER ## */}
       {/* <Notifications /> */}
       <RouterProvider router={router} />
